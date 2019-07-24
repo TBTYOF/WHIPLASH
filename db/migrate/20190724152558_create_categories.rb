7fbb1fcc8e75efd72e59e796662a8c13a6947f22
TBATYOF <toribatyof@gmail.com>
2019-07-25 00:36:33 +0900

    [Add]model,column,modelsにattachment2個追加

---
+++ db/migrate/20190724152558_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.integer :user_id
+      t.integer :blog_id
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
