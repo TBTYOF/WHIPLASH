d8c2744f314ecd3ae5b0abe128d4f6ecf0a6b02c
TBATYOF <toribatyof@gmail.com>
2019-07-25 15:08:17 +0900

    [Fix]ログインしていないとTopicに入れないbefore_actionを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :authenticate_user!, :except=>[:top, :about]
+	before_action :authenticate_user!, :except=>[:home, :about, :topic]
 
 	def after_sign_in_path_for(resource)
       blogs_path
