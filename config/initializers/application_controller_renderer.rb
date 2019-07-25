e0ff8594b597b7a766cb521d25978531dde18015
TBATYOF <toribatyof@gmail.com>
2019-07-25 16:10:14 +0900

    [Add]blog_showを表示完了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e75a174735ea6ae6024e65cd2e886ea214aa5cd4a2bd628e415b22c999e0c11abbac95dae6820fd45612fa722bf7b2e77fe87e275b632871fa136cb0a3f60ae8'
