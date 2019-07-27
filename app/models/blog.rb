46ec1302c9eea97f2457bbae50b217af31e3f397
TBATYOF <toribatyof@gmail.com>
2019-07-27 21:30:10 +0900

    [Add]favo実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,7 @@ class Blog < ApplicationRecord
 	attachment :blog_image
 	belongs_to :user
 	has_many :comments, dependent: :destroy
+	has_many :favorites, dependent: :destroy
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :blogs, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
