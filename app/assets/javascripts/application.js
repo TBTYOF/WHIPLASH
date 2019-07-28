cc8da48b2e48e4cfd97137a641c0d3c38945bf5c
TBATYOF <toribatyof@gmail.com>
2019-07-28 17:12:35 +0900

    [Add]ハンバーガーメニュー実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+	$('.menu-trigger').on('click', function(){
+		$(this).toggleClass('active');
+		$('#sp-menu').fadeToggle();
+		return false;
+	});
+});
+
