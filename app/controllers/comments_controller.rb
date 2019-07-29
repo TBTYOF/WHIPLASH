2054aaee6094ca1778787511c3bbfb7e786f9b0d
TBATYOF <toribatyof@gmail.com>
2019-07-29 19:48:00 +0900

    [Add]バリデーションエラー各ページに配置完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,20 @@
 class CommentsController < ApplicationController
 	def create
-	    blog = Blog.find(params[:blog_id])
-	    comment = current_user.comments.new(comment_params)
-	    comment.blog_id = blog.id
-	    comment.save
-	    redirect_to blog_path(blog.id)
+	    @blog = Blog.find(params[:blog_id])
+	    @comment = current_user.comments.new(comment_params)
+	    @comment.blog_id = @blog.id
+	    if @comment.save
+	    	redirect_to blog_path(@blog.id)
+	    else
+	    	@info_user = @blog.user
+		    @login_user = current_user
+		    @create_user = current_user
+		    @create_new = Blog.new
+		    @comments = @blog.comments.page(params[:page]).reverse_order
+		    @side_images = @info_user.blogs.order("RANDOM()").limit(10)
+	    	render 'blogs/show', info_user: @info_user, login_user: @login_user, create_user: @create_user,
+	    						create_new: @create_new, blog: @blog, comment: @comment, comments: @comments
+	    end
 	end
 
 	def destroy
